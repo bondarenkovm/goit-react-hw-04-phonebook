b85f6ca4209544a542476b29688784763daf1320
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-03-08 01:04:00 +0200

    +

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // import { Component } from 'react';
 import { Container } from './App.styled';
 import { ContactForm } from 'components/ContactForm';
@@ -7,31 +7,19 @@ import { ContactList } from 'components/ContactList';
 // import { testContacts } from 'data';
 import { Toaster } from 'react-hot-toast';
 import { toast } from 'react-hot-toast';
-import { useLocalStorage } from 'hooks';
-
-// function useLocalStorage(key, defaultArray) {
-//   const [state, setState] = useState(
-//     () => JSON.parse(window.localStorage.getItem(key)) ?? defaultArray
-//   );
-
-//   useEffect(() => {
-//     window.localStorage.setItem(key, JSON.stringify(state));
-//   }, [key, state]);
-
-//   return [state, setState];
-// }
+// import { useLocalStorage } from 'hooks';
 
 function App() {
-  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  // const [contacts, setContacts] = useLocalStorage('contacts', []);
 
-  // const [contacts, setContacts] = useState(
-  //   () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
-  // );
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem('contacts')) ?? []
+  );
   const [filter, setfilter] = useState('');
 
-  // useEffect(() => {
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   function formSubmitHandler(data) {
     if (contacts.find(contact => contact.name === data.name)) {
